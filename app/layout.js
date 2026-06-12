9f3dec3731513a2669af386126e22762356a8058
ihurah <nawaikanh@gmail.com>
2026-06-12 20:55:57 +0900

    ファビコン画像の変更

--- app/layout.js
+++ app/layout.js
@@ -22,8 +22,11 @@ export const metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico" },
+      { url: "/favicon_16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon_32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon_128.png", sizes: "128x128", type: "image/png" },
       { url: "/favicon_192.png", sizes: "192x192", type: "image/png" },
+      { url: "/favicon_512.png", sizes: "512x512", type: "image/png" },
     ],
     shortcut: "/favicon.ico",
     apple: "/favicon_192.png",
