42ef502956f5c535b1d559073d985e0d7cfc01a1
ihurah <nawaikanh@gmail.com>
2026-09-16 00:28:21 +0900

    fix: 転送設定:

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,25 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  async redirects() {
+    if (process.env.REDIRECT_SUBDOMAIN && process.env.REDIRECT_DESTINATION) {
+      return [
+        {
+          has: [
+            {
+              type: "host",
+              value: process.env.REDIRECT_SUBDOMAIN,
+            },
+          ],
+          source: "/:path*",
+          destination: process.env.REDIRECT_DESTINATION,
+          permanent: true,
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
