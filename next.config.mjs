5834b1a531d2c8ea0258d74d8b3d31d381e228fa
ihurah <nawaikanh@gmail.com>
2026-09-16 00:31:46 +0900

    fix: 転送設定;

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,18 @@ const nextConfig = {
   reactCompiler: true,
 
   async redirects() {
+    // 環境変数がVercel側で設定されている場合のみ動く（他の人がクローンしても無害）
     if (process.env.REDIRECT_SUBDOMAIN && process.env.REDIRECT_DESTINATION) {
       return [
         {
           has: [
             {
               type: "host",
-              value: process.env.REDIRECT_SUBDOMAIN,
+              value: process.env.REDIRECT_SUBDOMAIN, // Vercel上で「ha.ed1t.jp」を指定
             },
           ],
           source: "/:path*",
-          destination: process.env.REDIRECT_DESTINATION,
+          destination: process.env.REDIRECT_DESTINATION, // Vercel上で「https://ed1t.jp」を指定
           permanent: true,
         },
       ];
